cb6731e564318b3edbab47d8aebf26a72118c9e0
Aaron Elias <aaronelias007@gmail.com>
2022-10-21 22:43:42 -0400

    add create and show for user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user
+  end
+
+  def show
+    if current_user
+    render json: current_user
+    else 
+      render json: {error: "Not Authorized"}, status: :unauthorized
+    end 
+  end 
+
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,5 @@
 class User < ApplicationRecord
   
-  def create
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user
-  end
+  
 
-  def show
-    if current_user
-    render json: current_user
-    else 
-      render json: {error: "Not Authorized"}, status: :unauthorized
-    end 
-  end 
-
-
-  private
-
-  def user_params
-    params.permit(:username, :email, :password, :password_confirmation)
-  end
 end
