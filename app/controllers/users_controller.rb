b2f9f0527491f3a2e6b7c9e80db1bb00b667e1c9
Aaron Elias <aaronelias007@gmail.com>
2022-10-21 22:50:14 -0400

    Add bio to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:username, :email, :password, :password_confirmation)
+    params.permit(:username, :email, :password, :password_confirmation, :bio)
   end
 
 end
