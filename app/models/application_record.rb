d66ae2fc0620c997b04968b4dacaa3217806ae28
Aaron Elias <aaronelias007@gmail.com>
2022-10-21 22:36:07 -0400

    Re-add User Controller, Model, and Migration

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  include ActionController::Cookies
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
