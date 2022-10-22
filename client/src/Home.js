7fec3445fb5b87f6a0857229984706fb3d7919b6
Aaron Elias <aaronelias007@gmail.com>
2022-10-22 18:39:20 -0400

    Add Home Page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext } from './context/user'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+export default function Home() {
+   const { user } = useContext(UserContext)
+
+  if  (!user || user.error) {
+    return(<h1> Please Navigate to Login or Sign Up Form</h1>)
+  } else{
+      return (
+        <div>
+          <h2>Home Page for {user.username}</h2>
+          <h4>{user.bio}</h4>
+        </div>
+      )
+  }
 }
-
-export default Home
