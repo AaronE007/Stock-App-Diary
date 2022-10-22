a3da93ea6479bb3019fd804980bf5854f02967c4
Aaron Elias <aaronelias007@gmail.com>
2022-10-22 00:18:40 -0400

    Re-add Nav, Home, User

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import {Switch, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
+import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-  
+      <UserProvider>
+        <Navbar/>
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+        
+          </Routes>
+      </UserProvider>
     </div>
   );
 }
--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function LoginForm() {
+const LoginForm = () => {
   return (
     <div>
       
     </div>
   )
 }
+
+export default LoginForm
+
+
