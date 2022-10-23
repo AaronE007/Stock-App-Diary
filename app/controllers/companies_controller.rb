081b0a9f5b9d7e0b078e08933ca68db62b1f8837
Aaron Elias <aaronelias007@gmail.com>
2022-10-23 16:01:24 -0400

    Re-add Controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,20 @@
 class CompaniesController < ApplicationController
+  skip_before_action :authorized, only: [:index]
+  
+  def create
+    company = Company.create!(companies_params)
+    render json: company, status: :created
+  end
 
+  def index 
+    companies = Company.all
+    render json: companies 
+  end 
+
+
+  private 
+
+  def companies_params
+    params.require(:company).permit(:name, :description)
+  end
 end
