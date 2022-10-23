5e60ecaf99aa34e52fcf6b4c6266d901e55ab3b7
Aaron Elias <aaronelias007@gmail.com>
2022-10-23 18:57:25 -0400

    Re-add user serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,6 +1,6 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :bio
-
+  attributes :id, :name, :description
+  
   has_many :stocks
-  has_many :companies, through: :stocks
+  has_many :users, through: :stocks
 end
