06440d212fa6e839e904fdad38d7820f3ef350ed
Aaron Elias <aaronelias007@gmail.com>
2022-10-23 20:42:32 -0400

    Re-add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  has_many :stocks
+  has_many :companies, through: :stocks
+  
+  has_secure_password
 
+  validates :username, presence: true, uniqueness: true
 end
