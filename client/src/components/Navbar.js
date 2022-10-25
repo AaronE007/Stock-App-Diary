1b74cf3bfdc70f26e494a89c1620a80724146526
Aaron Elias <aaronelias007@gmail.com>
2022-10-25 15:09:56 -0400

    Add use Nav to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../context/user'
+import {useNavigate} from "react-router-dom"
 
 
 const Navbar = () => {
 
   const {user, logout, loggedIn} = useContext(UserContext)
+  const navigate = useNavigate()
+
+
   
 
   const userLogout = () => {
@@ -15,6 +19,7 @@ const Navbar = () => {
     })
     .then(() => {
       logout()
+      navigate('/')
     })
   }
 
