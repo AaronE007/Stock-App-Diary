eef2fffb66ab50059f0b7049b22baa2009c0e7b4
Aaron Elias <aaronelias007@gmail.com>
2022-10-25 19:21:54 -0400

    Finish stock edit

--- client/src/components/EditStock.js
+++ client/src/components/EditStock.js
@@ -1,10 +1,45 @@
 import React from 'react'
 
-const EditStock = () => {
+
+
+
+
+
+const EditStock = ({id, updateStock}) => {
+
+
+  const handleChange = () => {
+
+  }
+
+
   return (
-    <div>
-      <h2>Hello Me</h2>
-    </div>
+    <form onSubmit={handleFormSubmit}>
+    <label>Name: </label>
+    <input onChange={handleChange} type="text" name="name" value={stock.name} required/>
+    <br/>
+    <br/>
+
+    <label>Price purcahased at: </label>
+    <input onChange={handleChange} type="number" name="price_puchased_at" value={stock.price_purchases_at} required/>
+    <br/>
+    <br/>
+
+    <label>Number of stock bought: </label>
+    <input onChange={(handleChange)} type="number" name="number" value={stock.number} required/>
+    <br/>
+    <br/>
+
+    <label>Info: </label>
+    <input onChange={handleChange} type="text" name="info" value={stock.info} required/>
+    <br/>
+    <br/>
+
+    <label>Company bought from: </label>
+    <input onChange={handleChange} type="text" name="company" value={stock.company} required/>
+    <br/>
+    <input type="submit" value="Change Stock Buy Data" />
+    </form>
   )
 }
 
